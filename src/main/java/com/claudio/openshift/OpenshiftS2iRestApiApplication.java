4302285609364b139ec526af3ff7273ec9a42a1a
Claudio Cifuentes <cacifuentesalonso@gmail.com>
2021-11-09 14:58:05 -0600

    Initial commit

---
+++ src/main/java/com/claudio/openshift/OpenshiftS2iRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.claudio.openshift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenshiftS2iRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenshiftS2iRestApiApplication.class, args);
+	}
+
+}
